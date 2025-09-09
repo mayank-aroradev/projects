cd8309ceb4df8730f99719084c129d05fe436ee8
Mayank Arora <mayankarora25071@gmail.com>
2025-09-10 00:08:33 +0530

    snakw class

--- snake game/snake.py
+++ snake game/snake.py
@@ -1,6 +1,7 @@
-from turtle import Turtle
+from turtle import Turtle, position
 
 STARTING_POSITIONS = [(0,0), (-20,0), (-40,0)]
+position =  [(0,0), (-20,0)]
 MOVE_DISTANCE = 20
 UP=90
 Down=270
@@ -16,11 +17,18 @@ class Snake:
     
     def create_snake(self):
         for positon in STARTING_POSITIONS:
+            self.add_segment(position)
+
+    def add_segment(self,position):
             new_segment = Turtle("square")
             new_segment.color("white")
             new_segment.penup()
-            new_segment.goto(positon)
+            new_segment.goto(position)
             self.segments.append(new_segment)
+    
+    def extend(self):
+        self.add_segment(self.segments[-1].position())
+
 
     def move (self):
          for seg_num in range(len(self.segments)-1, 0, -1):
