994b3e1ba8c698fa9acca8652b1a40d7959874d5
Mayank Arora <mayankarora25071@gmail.com>
2025-10-16 21:25:36 +0530

    main class

--- ping pong/main.py
+++ ping pong/main.py
@@ -1,5 +1,9 @@
 from turtle import Screen, Turtle
 from paddle import Paddle 
+from ball import Ball
+import time 
+
+
 screen = Screen()
 screen.bgcolor("black")
 screen.setup(width=800, height= 600)
@@ -8,6 +12,7 @@ screen.tracer(0)
 
 r_paddle = Paddle((350,0))
 l_paddle = Paddle((-350,0))
+ball = Ball()
 
 
 
@@ -21,7 +26,28 @@ screen.onkey(l_paddle.go_down,"s")
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
+    #detect collision with wall
+    if ball.ycor()>280 or ball.ycor()<-280:
+        ball.bounce_y()
+
+    #detection with paddle 
+    if ball.distance(r_paddle) < 50 and ball.xcor()>320 or  ball.distance(l_paddle) < 50 and ball.xcor()>-320:
+        ball.bounce_x()
+
+    #detect when right paddle misses
+    if ball.xcor() > 380:
+        ball.reset_position()
+
+    if ball.xcor()<-380:
+        ball.reset_position()
+
+
+
+
+
     
 
 screen.exitonclick()
