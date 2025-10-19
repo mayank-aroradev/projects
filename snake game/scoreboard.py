76e503b636b3aed766b138867d2f98f4872b66c2
Mayank Arora <mayankarora25071@gmail.com>
2025-10-19 20:09:43 +0530

    scoreboard

--- snake game/scoreboard.py
+++ snake game/scoreboard.py
@@ -3,6 +3,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0,270)
@@ -10,14 +11,21 @@ class Scoreboard(Turtle):
         self.hideturtle()
 
     def update_scoreboard(self):
-            self.write(f"score: {self.score}", align="left",font=("Arial",24,"normal"))
+            self.clear()
+            self.write(f"score: {self.score} HIGH SCORE: {self.high_score}" , align="center",font=("Arial",24,"normal"))
+
+    def reset(self):
+         if self.score > self.high_score:
+              self.high_score = self.score
+         self.score = 0
+         self.update_scoreboard()
+
+
         
-    def game_over(self):
-         self.goto(0,0)
-         self.write("GAME OVER")
+    
 
     def increase_score(self):
         self.score += 1 
-        self.clear()
+        
         self.update_scoreboard()
         
