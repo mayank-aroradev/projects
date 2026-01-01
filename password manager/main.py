5cf83e0df5663b6f60b4fb16a5ec32a978aaac27
Mayank Arora <mayankarora25071@gmail.com>
2026-01-01 14:10:02 +0530

    new

--- password manager/main.py
+++ password manager/main.py
@@ -1,5 +1,27 @@
 from tkinter import *
 from tkinter import messagebox
+import pyperclip
+
+
+def generate():
+    import random
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    print("Welcome to the PyPassword Generator!")
+    nr_letters = random.randint(0,10)
+    nr_symbols = random.randint(2,4)
+    nr_numbers = random.randint(2,4)
+    password_letters =[random.choice(letters) for _ in range(nr_letters)]
+    password_symbol = [random.choice(symbols) for _ in range(nr_symbols)]
+    password_numbers =[random.choice(numbers) for _ in range (nr_numbers)]
+
+    password_list = password_letters + password_symbol + password_numbers
+    random.shuffle(password_list)
+    password = "".join(password_list) 
+
+    password_entry.insert(0, password)
 
  
 def save():
@@ -7,13 +29,19 @@ def save():
     website = website_entry.get()
     email = email_entry.get()
     password = password_entry.get()
-    
-    messagebox.askokcancel(title=website, message= f"these are the details entered: \n")
 
-    with open(r"C:\Users\dell\Desktop\projects\password manager\data.txt", "a") as data_files:
-        data_files.write(f"{website} || {email} || {password}\n")
-        website_entry.delete(0, END)
-        password_entry.delete(0,END)
+    if len(website)== 0 or len(password)==0:
+        messagebox.showinfo(title= "oops", message="please make sure you havent left any field")
+    else:   
+    
+        is_ok= messagebox.askokcancel(title=website, message= f"these are the details entered: \nEmail:{email}"
+                           f"\npassword:{password} \nIs it ok to save?")
+    if is_ok:
+
+        with open(r"C:\Users\dell\Desktop\projects\password manager\data.txt", "a") as data_files:
+            data_files.write(f"{website} || {email} || {password}\n")
+            website_entry.delete(0, END)
+            password_entry.delete(0,END)
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Password Manager")
@@ -48,10 +76,10 @@ password_entry = Entry(width=35)
 password_entry.grid(row=3, column=1,columnspan=2)
 
 # Buttons
-generate_password_button = Button(text="Generate Password")
+generate_password_button = Button(text="Generate Password",width= 20, command= generate)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command= save )
 add_button.grid(row=4, column=1, columnspan=2)
 
-window.mainloop()
+window.mainloop() 
