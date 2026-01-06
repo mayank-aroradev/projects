f603227f217e7aa90a8c9826b58a4695bcde54c5
Mayank Arora <mayankarora25071@gmail.com>
2026-01-06 23:13:17 +0530

    environment variable

--- rainalert/main.py
+++ rainalert/main.py
@@ -1,9 +1,21 @@
 import requests
-api_key = "f44a758757100ac23e882daf2974ffd1"
+import os
+api_key = os.environ.get("API_KEY")
 parameters={
     "lat": 28.6619,
     "lon": 77.2273,
     "appid": api_key,
+    "cnt":4,
 }
-response= requests.get("https://api.openweathermap.org/data/2.5/onecall",params=parameters)
-print(response.status_code)
+response= requests.get("https://api.openweathermap.org/data/2.5/forecast",params=parameters)
+response.raise_for_status()
+data=response.json()
+will_rain = False
+for hour_data in data["list"]:
+    condition_code=hour_data["weather"][0]["id"]
+    if int(condition_code)<700:
+        will_rain=True
+if will_rain:
+    print("Bring an umbrella")
+else:
+    print("No rain expected")
