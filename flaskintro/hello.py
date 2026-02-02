07913560490d5d1e359106f610e32376d33e64f4
Mayank Arora <mayankarora25071@gmail.com>
2026-02-02 23:13:56 +0530

    python deco

--- flaskintro/hello.py
+++ flaskintro/hello.py
@@ -4,8 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, daddy!"
+    return "<h2>Hello, daddy!</h2>"
+@app.route("/username/<name>")
+def greet(name):
+    return f"hello {name}!"
 
+@app.route("/username/<name>/<int:number>")
+def greetme(name,number):
+    return "Hello there {name},your age is {number} year old!"
 if __name__ == "__main__":
     app.run(debug=True)
- 
+    
