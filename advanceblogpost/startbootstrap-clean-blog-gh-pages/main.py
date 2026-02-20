b27ea243d52f3fb3de8e79456975e3921d21f9ab
Mayank Arora <mayankarora25071@gmail.com>
2026-02-20 23:43:18 +0530

    type of a review

--- advanceblogpost/startbootstrap-clean-blog-gh-pages/main.py
+++ advanceblogpost/startbootstrap-clean-blog-gh-pages/main.py
@@ -1,8 +1,12 @@
-from flask import Flask , render_template
+from flask import Flask , render_template,request
 import requests
+import smtplib
 from post import Post
 import datetime as dt
-
+import os 
+import smtplib
+OWN_EMAIL=os.getenv('OWN_EMAIL')
+OWN_PASSWORD=os.getenv('OWN_PASSWORD')
 year=dt.datetime.now().year
 
 
@@ -25,9 +29,34 @@ def show_post(index):
         if blog_post["id"] == index:
             return render_template("post.html", post=blog_post, yer=year)
 
-@app.route("/contact" , methods=["POST"])
+@app.route("/datame" , methods=["GET","POST"])
 def datame():
-    return 'success'
+   
+    if request.method=="POST":
+        data = request.form
+        user_name=(data["name"])
+        user_email=(data["email"])
+        user_number=(data["phone"])
+        user_message=(data["message"])
+        return render_template("success.html",)
+        
+
+        reciever_email=OWN_EMAIL
+        password= OWN_PASSWORD
+        message=f"Name:{user_name}\n Phone:{user_number}\nHELLO THERE\n{user_message}"
+        msg = EmailMessage()
+        msg.set_content(message)
+        msg['subject']='you are the best'
+        msg['from']=user_email
+        msg['To']=recievers_email
+
+
+        with smtplib.SMTP(host="smtp.gmail.com",port=587) as connection:
+            connection.starttls()
+            connection.login(receivers_email, password)
+            connection.sendmail(OWN_EMAIL,OWN_EMAIL,email_message)
+        return render_template("contact.html",msg_sent=True)
+    return render_template('contact.html',msg_sent=False)
 
 
 
